0474158a787b6ec2efa9e01b89271ae4a6817f53
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-07-03 17:08:35 -0300

    Add a method to print the options in the cli

---
+++ helpers/print_options.rb
@@ -0,0 +1,13 @@
+def print_options
+  system "clear"
+
+  puts "---Breaking Bad characters info---"
+  
+  puts "\nChoose between the options bellow:"
+  puts "1 - Get all Characters information;"
+  puts "2 - Get single Character information by ID;"
+  puts "3 - Get single Character information by name;"
+  puts "4 - Get random Character information;"
+  puts "0 - Exit."
+  print "Option = "
+end
